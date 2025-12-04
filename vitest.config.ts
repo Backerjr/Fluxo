19249310ada9ddbb6dc59a773361211a224de0b2
Ahmed Backer <ahmedbackerjr@gmail.com>
2025-12-04 12:58:45 +0400

    Update alias path for '@' in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   root: templateRoot,
   resolve: {
     alias: {
-      "@": path.resolve(templateRoot, "src"),
+      "@": path.resolve(templateRoot, "client", "src"),
       "@shared": path.resolve(templateRoot, "shared"),
       "@assets": path.resolve(templateRoot, "attached_assets"),
     },
